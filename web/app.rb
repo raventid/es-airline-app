5c5786aa3b95c0c79a63fc19dad10f93f2f532cb
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-10-13 22:28:57 +0300

    refactor config.ru, move web-app to app.rb

---
+++ web/app.rb
@@ -0,0 +1,30 @@
+require "roda"
+
+class App < Roda
+  route do |r|
+    # GET / request
+    r.root do
+      r.redirect "ping"
+    end
+
+    # GET /search_fare request
+    r.on "search_fare" do
+      puts "run component responsible for search_fare"
+    end
+
+    # GET /reserving_seats request
+    r.on "reserving_seats" do
+      puts "run component responsible for reserving_seats"
+    end
+
+    # GET /issue_ticket request
+    r.on "issue_ticket" do
+      puts "run component responsible for issue_ticket"
+    end
+
+    # GET /ping request
+    r.on "ping" do
+      "pong"
+    end
+  end
+end
