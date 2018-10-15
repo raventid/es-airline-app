1db13381bbb3356057ff125ac3837a348a7dbdf1
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-10-15 14:54:13 +0300

    adds wolfgang web-app implementation draft (without external deps)

---
+++ external_applications/wolfgang.rb
@@ -0,0 +1,53 @@
+require "webrick"
+require "json"
+
+class SearchFareServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(request, response)
+    puts request
+    response.content_type = "application/json"
+    response.status = 200
+    response.body = payload
+  end
+
+  def payload
+    { flight: {}, prices: {}, branded_fare: {} }.to_json
+  end
+end
+
+class ReservingSeatsServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(request, response)
+    puts request
+    response.content_type = "application/json"
+    response.status = 200
+    response.body = payload
+  end
+
+  def payload
+    { flight: {}, prices: {}, branded_fare: {} }.to_json
+  end
+end
+
+class IssueTicketServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(request, response)
+    puts request
+    response.content_type = "application/json"
+    response.status = 200
+    response.body = payload
+  end
+
+  def payload
+    { flight: {}, prices: {}, branded_fare: {} }.to_json
+  end
+end
+
+server = WEBrick::HTTPServer.new(Port: 8081)
+
+server.mount "/search_fare", SearchFareServlet
+server.mount "/reserving_seats", ReservingSeatsServlet
+server.mount "/issue_ticket", IssueTicketServlet
+
+trap "INT" do
+  server.shutdown
+end
+
+server.start
