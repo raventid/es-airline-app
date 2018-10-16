fddeae372a8c5b5e92fcbaee3d3d002d42d11a6f
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-10-16 17:46:15 +0300

    add refund concept to project

---
+++ refunds/app.rb
@@ -0,0 +1,11 @@
+# I get a refund command from user.
+
+# I'm actuating the component reponsible for refund.
+
+# This component decide which GDS gateway to use and?
+# - Should it spawn new command to this gateway
+# - Should it write event to stream, that he chooses this gateway to do smth?
+
+# After this moment this component is not interested in gateway actions
+
+# It might be interested in some other events, though.
