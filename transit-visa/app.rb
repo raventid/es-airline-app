3c1ddc7179b7fc06c4fbf2ac99b3b77b8c22e75e
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-10-19 20:31:30 +0300

    start fleshing out transit visa

--- transit-visa/app.rb
+++ transit-visa/app.rb
@@ -1,10 +1,44 @@
 # We might have a lot of services like that
 # which should react to different events in processes
+
+class TransitVisaRequired
+  def call
+    {
+      reason: "flight from Russia to USA with transition in Germany"
+    }
+  end
+end
+
+class TransitVisaIsNotRequired
+  def call
+    {
+    }
+  end
+end
+
 class App
   def handle(event)
     case event
-    when BookedTicket
-      need_visa?
+    when NewOfferCreated
+      result = need_visa?
+
+      if result
+        event = TransitVisaRequired.new.call
+        publish_event(need_visa)
+      else
+        event = TransitVisaIsNotRequired.new.call
+        publish_event(need_visa)
+      end
+    when FlightDataHasChanged
+      result = need_visa?
+
+      if result
+        event = TransitVisaRequired.new.call
+        publish_event(need_visa)
+      else
+        event = TransitVisaIsNotRequired.new.call
+        publish_event(need_visa)
+      end
     end
   end
 
