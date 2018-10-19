ee319c52b499533fca9e583f6a7796f58ef8a90e
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-10-20 00:19:33 +0300

    hidden fare component should react reserved ticket (for now)

--- hidden-fare/app.rb
+++ hidden-fare/app.rb
@@ -1,30 +1,31 @@
+class HiddenFareFound
+  def call
+    {
+      price: 1000
+    }
+  end
+end
+
+class HiddenFareNotFound
+  def call
+    {
+      reason: "was not able to find one"
+    }
+  end
+end
+
 class HiddenFare
   def handle(event)
     case event
-    when SearchFare
-      PerformSearchFare.new.call
-    when ReserveSeats
-      PerformReserveSeats.new.call
-    when IssueTicket
-      PerformIssuingTicket.new.call
-    end
-  end
-
-  class PerformSearchFare
-    def call
-      puts "Searching fare"
-    end
-  end
-
-  class PerformReserveSeats
-    def call
-      puts "Reserving seats"
-    end
-  end
-
-  class PerformIssuingTicket
-    def call
-      puts "Issuing ticket"
+    when TicketReserved
+      result = hidden_fare_api.check_for(order)
+      if result
+        event = HiddenFareFound.new.call
+        publish_event(event)
+      else
+        event = HiddenFareNotFound.new.call
+        publish_event(event)
+      end
     end
   end
 end
