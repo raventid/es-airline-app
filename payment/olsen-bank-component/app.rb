9058e9e398398995e56b8e0bf8b31bf96cc8c146
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-10-19 20:30:17 +0300

    start fleshing out olsen bank

--- payment/olsen-bank-component/app.rb
+++ payment/olsen-bank-component/app.rb
@@ -1,14 +1,34 @@
+class PaymentCompleted
+  def call
+    {
+      amount_paid: 1000,
+      credit_left: 10000,
+    }
+  end
+end
+
+class PaymentFailed
+  def call
+    {
+      reason: { "Bank rejected the card with error 1054" }
+    }
+  end
+end
+
 class OlsenBank
   def handle(event)
     case event
     when PayForOrder
-      PerformPayForOrder.new.call
-    end
-  end
+      amount = extract_payment_amount
+      result = bank.api.pay(amount)
 
-  class PerformPayForOrder
-    def call
-      puts "Perform pay for order"
+      if result
+        event = PaymentCompleted.new.call
+        publish_event(event)
+      else
+        event = PaymentFailed.new.call
+        publish_event(event)
+      end
     end
   end
 end
