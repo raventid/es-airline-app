092c3730ece79d6f9f459cb5f8a242d771ed2174
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-10-19 20:29:57 +0300

    remove deprecated events

--- passengers-monitoring/app.rb
+++ passengers-monitoring/app.rb
@@ -1,38 +1,11 @@
+class PassengerStateMonitored
+end
+
 class PassengersMonitoring
   def handle(event)
     case event
-    when SearchFare
-      PerformSearchFare.new.call
-    when ReserveSeats
-      PerformReserveSeats.new.call
-    when IssueTicket
-      PerformIssuingTicket.new.call
-    when PayForOrder
-      PerformPayForOrder.new.call
-    end
-  end
+    when ReserveSeats, IssueTicket, OrderPaid
 
-  class PerformSearchFare
-    def call
-      puts "Passenger successfuly got new fare price"
-    end
-  end
-
-  class PerformReserveSeats
-    def call
-      puts "Seat successfuly reserved for passenger"
-    end
-  end
-
-  class PerformIssuingTicket
-    def call
-      puts "Ticket was issued for passenger"
-    end
-  end
-
-  class PerformPayForOrder
-    def call
-      puts "Payment successfuly performed and accepted"
     end
   end
 end
