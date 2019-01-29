a24384a1a0e3620018159734980aa72282a16f60
Julian Pokrovsky <juliankul@gmail.com>
2019-01-29 21:21:00 +0300

    example search_fare handler for wolfgang gds

--- wolfgang-component/lib/wolfgang_component/handlers/commands.rb
+++ wolfgang-component/lib/wolfgang_component/handlers/commands.rb
@@ -24,7 +24,7 @@ module WolfgangComponent
 
       category :wolfgang
 
-      # TODO Implement command handler blocks"
+      # TODO Implement command handler blocks
       # eg:
       # handle DoSomething do |do_something|
       #   wolfgang_id = do_something.wolfgang_id
@@ -44,6 +44,34 @@ module WolfgangComponent
 
       #   write.(something_happened, stream_name, expected_version: version)
       # end
+      handle FindFare do |find_fare|
+        wolfgang_id = find_fare.wolfgang_id
+
+        # skip idempotence protections
+        store.fetch(wolfgang_id)
+
+        search_fare_response = run_external_idepotent_search_fare_request(find_fare.req_payload)
+
+        # if fare is successfully found
+        if search_fare_response.success? && search_fare_response.prices
+          fare_found = FareFound.follow(find_fare)
+          write.(fare_found, stream_name)
+        end
+
+        # if fare is not found at all
+        if !search_fare_response.success?
+          fare_not_found = FareNotFound.follow(find_fare)
+          write.(fare_not_found, stream_name)
+        end
+
+        # Both of the messages above are also handled by search-fare-component.
+        #
+        #
+        #
+
+        # timeouts?
+        # internal response (bad xml parsing, credentials mistake)
+      end
     end
   end
 end
