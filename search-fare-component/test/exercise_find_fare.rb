c6cafee2127a0dbf0c89e56b93305178619e3aa6
Julian Pokrovsky <juliankul@gmail.com>
2019-02-01 14:03:04 +0300

    basic spec for search_fare component

---
+++ search-fare-component/test/exercise_find_fare.rb
@@ -0,0 +1,25 @@
+require_relative '../init.rb'
+
+module SearchFareComponent; end
+include SearchFareComponent
+
+search_fare_id = Identifier::UUID::Random.get
+find_fare = Messages::Commands::FindFare.new
+find_fare.search_fare_id = search_fare_id
+find_fare.time = '2000-01-01T11:11:11.000Z'
+find_fare.time = {
+  "mow-led": {
+    departure: "MOW",
+    arrival: "LED",
+    gds: "charter",
+  },
+  "led-mow": {
+    departure: "LED",
+    arrival: "MOW",
+    gds: "wolfgang"
+  }
+}
+
+command_stream_name = "searchFare:command-#{search_fare_id}"
+
+Messaging::Postgres::Write.(find_fare, command_stream_name)
