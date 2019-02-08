f68a0fb66efff405c71bc9ec1d238dc1b97edb72
Julian Pokrovsky <juliankul@gmail.com>
2019-02-08 18:14:56 +0300

    explanation of what status field is doing

--- order-component/lib/order_component/order.rb
+++ order-component/lib/order_component/order.rb
@@ -7,6 +7,19 @@ module OrderComponent
 
     attribute :id, String
     attribute :pnr, String
+
+    # This field acts as status(state) machine
+    # Possible values are:
+    # - created
+    # - fare_found
+    # - ticket_booked
+    # - order_paid
+    #
+    # Also it's possible to have broken states:
+    # - fare_fault
+    # - payment_failure
+    #
+    # So, in some sense this field is a materialization of work by whole system.
     attribute :status, String
 
     # Note: This class's methods should pertain only to its attributes
